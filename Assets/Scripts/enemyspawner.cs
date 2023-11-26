d5c5889b8c578e2e7bf466b5f4fac1d5831b3441
akhilrnath <151564050+akhilrnath@users.noreply.github.com>
2023-11-26 01:05:33 +0000

    enemy partial

---
+++ Assets/Scripts/enemyspawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyspawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    public float spawnRate = 2;
+    public float timer = 0;
+    public float HeightOffset = 10;
+    void Start()
+    {
+        spawnenemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnenemy();
+            timer = 0;
+        }
+    }
+    void spawnenemy()
+    {
+        float lowestpoint = transform.position.y - HeightOffset;
+        float Highestpoint = transform.position.y  + HeightOffset;
+        Instantiate(enemy, new Vector3(transform.position.x,Random.Range(lowestpoint,Highestpoint),0),transform.rotation);
+    }
+}
